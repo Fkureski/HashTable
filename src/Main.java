dd12f46d895a99727a9ffc2bff6de8aa5fac823c
Fkureski <felipekureski@gmail.com>
2025-05-24 17:13:11 -0300

    File reader working using utils

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import utils.TxtReader;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        TxtReader leitor = new TxtReader();
+        List<String> nomes = leitor.lerNomesDeArquivo("female_names.txt");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Total de nomes lidos: " + nomes.size());
+        nomes.stream().limit(2500).forEach(System.out::println);
         }
-    }
 }
---
+++ src/utils/TxtReader.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TxtReader {
+
+    public List<String> lerNomesDeArquivo(String nomeArquivo) {
+        List<String> nomes = new ArrayList<>();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("utils/" + nomeArquivo);
+        if (inputStream == null) {
+            System.err.println("Arquivo não encontrado: " + nomeArquivo);
+            return nomes;
+        }
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                nomes.add(linha.trim());
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+
+        return nomes;
+    }
+}
