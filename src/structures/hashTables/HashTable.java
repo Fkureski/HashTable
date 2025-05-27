badca4ab7b7ac6abfb3d47f35f49a7c6560da318
Tiago Prestes <bptiago123@gmail.com>
2025-05-27 09:39:40 -0300

    Add funções de resize e remoção

--- src/structures/hashTables/HashTable.java
+++ src/structures/hashTables/HashTable.java
@@ -3,9 +3,12 @@ package structures.hashTables;
 import structures.ArrayList;
 
 public abstract class HashTable {
-    protected int capacidade = 32;
-    protected ArrayList<String>[] tabela;
+    protected final int CAPACIDADE_MAXIMA = 32;
+    protected final double LIMITE_FATOR_CARGA = 0.75;
+    protected int capacidade = 8;
+    protected int totalElementos = 0;
     protected int totalColisoes = 0;
+    protected ArrayList<String>[] tabela;
 
     @SuppressWarnings("unchecked")
     public HashTable() {
@@ -19,6 +22,10 @@ public abstract class HashTable {
     protected abstract int hash(String chave);
 
     public void inserir(String chave) {
+        if ((double) totalElementos / capacidade >= LIMITE_FATOR_CARGA && capacidade < CAPACIDADE_MAXIMA) {
+            redimensionar();
+        }
+
         int pos = hash(chave);
 
         if (!tabela[pos].isEmpty()) {
@@ -26,6 +33,7 @@ public abstract class HashTable {
         }
 
         tabela[pos].add(chave);
+        totalElementos++;
     }
 
     public boolean buscar(String chave) {
@@ -34,6 +42,20 @@ public abstract class HashTable {
         for (int i = 0; i < bucket.toArray().length; i++) {
             if (bucket.get(i).equals(chave)) return true;
         }
+        return false;
+    }
+
+    public boolean remover(String chave) {
+        int pos = hash(chave);
+        ArrayList<String> bucket = tabela[pos];
+        for (int i = 0; i < bucket.toArray().length; i++) {
+            if (bucket.get(i).equals(chave)) {
+                bucket.remove(i);
+                totalElementos--;
+                return true;
+            }
+        }
+
         return false;
     }
 
@@ -58,5 +80,30 @@ public abstract class HashTable {
         } catch (IndexOutOfBoundsException e) {
             return count;
         }
+    }
+
+    private void redimensionar() {
+        int novaCapacidade = Math.min(capacidade * 2, CAPACIDADE_MAXIMA);
+        ArrayList<String>[] novaTabela = new ArrayList[novaCapacidade];
+        for (int i = 0; i < novaCapacidade; i++) {
+            novaTabela[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < capacidade; i++) {
+            ArrayList<String> bucket = tabela[i];
+            for (int j = 0; j < bucket.toArray().length; j++) {
+                String chave = bucket.get(j);
+                int novaPos = hash(chave) % novaCapacidade;
+
+                if (!novaTabela[novaPos].isEmpty()) {
+                    totalColisoes++;
+                }
+
+                novaTabela[novaPos].add(chave);
+            }
+        }
+
+        tabela = novaTabela;
+        capacidade = novaCapacidade;
     }
 }
